d49b716dc5e487961e1636db9a0cd10e60dea2ed
Mr Booi <ayabongabooi2@gmail.com>
2018-06-09 18:05:00 +0200

    completed add function and getAL reg function

--- Registration_Numbers.js
+++ Registration_Numbers.js
@@ -1,69 +1,69 @@
 // factory function
-module.exports=function(storedRegNumbers) {
+module.exports = function (pool) {
   var regNumber = "";
   var RegNumberMap = {};
 
-  if (storedRegNumbers) {
-    for (var i = 0; i < storedRegNumbers.length; i++) {
-      let regStored = storedRegNumbers[i];
-      RegNumberMap[regStored] = 0;
+
+
+
+  async function setReg(value) {
+    // if (value !== "" && value.length > 0 && value.startsWith("CA ") || value.startsWith("CL") || value.startsWith("CAW") || value.startsWith("CJ")) {
+    //   return false;
+    // }
+    var result = await pool.query('SELECT * FROM registrationNo WHERE reg_number=$1', [value]);
+    console.log(result.rowCount)
+    if (result.rowCount === 0) {
+      let townTag = value.substring(0, 3).trim();
+      console.log(townTag);
+      let getid= await pool.query('SELECT id FROM towns WHERE town_tag=$1',[townTag]);
+      await pool.query('INSERT INTO registrationNo (reg_number,town) VALUES ($1,$2)',[value,getid.rows[0].id]);
+      
+      return true;
     }
   }
 
+  // getmap function
+  async function getRegistrationMap() {
+    let registration = await pool.query('SELECT reg_number fROM registrationNo');
+    return registration.rows;
+  }
+  // get registration number
+  async function getRegistationNumber() {
 
- async function setReg(value) {
-    if (RegNumberMap[value] === undefined) {
-      if (value !== "" && value.length > 0 && value.startsWith("CA ") || value.startsWith("CL") || value.startsWith("CAW") || value.startsWith("CJ")) {
-        regNumber = value;
-        RegNumberMap[regNumber] = 0;
-
-        return true;
-      }
-      return false;
-
-    }
+    return regNumber;
   }
 
-    // getmap function
-   async function getRegistrationMap(storedRegNumbers) {
-      return Object.keys(RegNumberMap);
-    }
-    // get registration number
-   async function getRegistationNumber() {
-      return regNumber;
-    }
- 
-    // filterby function
-  async  function filterBy(filterTown) {
+  // filterby function
+  async function filterBy(filterTown) {
 
-      var regNums = Object.keys(RegNumberMap);
-      if (filterTown === 'All') {
-        return regNums;
-      }
-      var townFilter = regNums.filter(function(regNumber) {
+    var regNums = Object.keys(RegNumberMap);
+    if (filterTown === 'All') {
+      return regNums;
+    }
+    var townFilter = regNums.filter(function (regNumber) {
 
-        return regNumber.startsWith(filterTown)
-      });
-   
-      return townFilter;
-    }
-    // get selected Town
-   async function getSelectedTownList() {
-      return tempTown;
-    }
-   
-  async  function clear(){
-     let clear= RegNumberMap ={} ;
-      return clear ;
-    }
+      return regNumber.startsWith(filterTown)
+    });
 
-    // returning all functions inside a factory function
-    return {
-      setRegistration: setReg,
-      getMap: getRegistrationMap,
-      getRegNumber: getRegistationNumber,
-      filterTowns: filterBy,
-      getListSelectedTown: getSelectedTownList,
-      clear
-    }
+    return townFilter;
   }
+  // get selected Town
+  async function getSelectedTownList() {
+    return tempTown;
+  }
+
+  async function clear() {
+    let clear = RegNumberMap = {};
+    return clear;
+  }
+
+  // returning all functions inside a factory function
+  return {
+    setRegistration: setReg,
+    getMap: getRegistrationMap,
+    getRegNumber: getRegistationNumber,
+    filterTowns: filterBy,
+    getListSelectedTown: getSelectedTownList,
+    clear
+  }
+}
--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const Registration = require('./Registration_Numbers');
 const app = express();
 var PORT = process.env.PORT || 3000;
 
-//   const pg = require('pg');
-//   const Pool = pg.Pool;
+const pg = require('pg');
+const Pool = pg.Pool;
 
 let useSSL = false;
 if (process.env.DATABASE_URL) {
@@ -18,12 +18,12 @@ if (process.env.DATABASE_URL) {
 
 const connectionString = process.env.DATABASE_URL || 'postgresql://coder:coder123@localhost:5432/registrationNumbers'
 
-//  const pool = new Pool({
-//   connectionString,
-//   ssl: useSSL
-//  });
+const pool = new Pool({
+    connectionString,
+    ssl: useSSL
+});
 
-const registration_numbers = Registration();
+const registration_numbers = Registration(pool);
 
 app.use(express.static('public'));
 
@@ -38,37 +38,60 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
-app.get("/", function (req, res) {
-    res.render('reg_number');
+app.get("/", async function (req, res, next) {
+    try {
+        let reglist = await registration_numbers.getMap();
+        res.render('reg_number', { reglist });
+    } catch (err) {
+        next(err);
+    }
+
 });
 
-app.get("/reg_number/:numberPlate", function (req, res) {
-    let numberPlate = req.params.numberPlate;
-    registration_numbers.setRegistration(numberPlate);
-    res.render('reg_number', {reg_numberList:registration_numbers.getMap()});
+app.get("/reg_number/:numberPlate", async function (req, res, next) {
+    try {
+        let numberPlate = req.params.numberPlate;
+        await registration_numbers.setRegistration(numberPlate);
+        let reglist = await registration_numbers.getMap();
+        res.render('reg_number', { reglist });
+    } catch (err) {
+        next(err);
+    }
 });
 
-app.post("/reg_number", function (req, res) {
-    let numberPlate = req.body.enteredReg;
-    registration_numbers.setRegistration(numberPlate);
-    res.render('reg_number', {reg_numberList:registration_numbers.getMap()});
+app.post("/reg_number", async function (req, res, next) {
+    try {
+        let numberPlate = req.body.enteredReg;
+        await registration_numbers.setRegistration(numberPlate);
+        let reglist = await registration_numbers.getMap();
+        // res.render('reg_number', { reglist });
+        res.redirect('/');
+    } catch (err) {
+        next(err);
+    }
 });
 
 
 
-app.get('/filter/:tag',function(req,res){
-    let city =req.params.tag;
-    let filterbyTown=registration_numbers.filterTowns(city);
-
-  res.render('reg_number',{reg_numberList:filterbyTown});
+app.get('/filter/:tag', async function (req, res, next) {
+    try {
+        let city = req.params.tag;
+        let filterbyTown = registration_numbers.filterTowns(city);
+        res.render('reg_number', { reg_numberList: filterbyTown });
+    } catch (err) {
+        next(err);
+    }
 });
 
 
-app.get('/reset',function(req,res){
-  
-    registration_numbers.clear();
- res.redirect("/");
- });
+app.get('/reset', async function (req, res, next) {
+    try {
+        registration_numbers.clear();
+        res.redirect("/");
+    } catch (err) {
+        next(err)
+    }
+});
 
 
 app.listen(PORT, function (err) {
