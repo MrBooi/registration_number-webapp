6031760628466c67b80d834d00da9343769c0910
Mr Booi <ayabongabooi2@gmail.com>
2018-06-12 09:42:56 +0200

    fixing styling

--- registrationdb-script.sql
+++ registrationdb-script.sql
@@ -1,5 +1,5 @@
-drop table towns;
-drop table registrationNo;
+
+drop table if exists towns,registrationNo;
 
 create table towns (
 id serial not null primary key,
@@ -13,3 +13,11 @@ CREATE TABLE registrationNo(
  town int not null,
   foreign key (town) references towns(id)
 );
+
+
+
+
+insert into towns(town_name,town_tag) values ('Cape Town','CA');
+insert into towns(town_name,town_tag) values ('Stellenbosch','CL');
+insert into towns(town_name,town_tag) values ('Paarl','CJ');
+insert into towns(town_name,town_tag) values ('George','CAW');
